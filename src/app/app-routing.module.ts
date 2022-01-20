462d0568814e8e85f109b3359555819a1091807a
mBaenaCh <mateo.baena@hotmail.com>
2022-01-20 09:39:57 -0500

    Feature: Technician routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TechnicianComponent } from './components/technician/technician.component';
 
 // here, put all routes that you need use.
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', pathMatch: 'full', redirectTo: 'technician'},
+  {path: 'technician', component: TechnicianComponent},
+  {path: '**', redirectTo: '/'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
